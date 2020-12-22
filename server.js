e1d1c68e5c2e5b0da04f3dc1e8c4fbbeb86f4e27
serg-kanaev <75146362+serg-kanaev@users.noreply.github.com>
2020-12-22 21:28:35 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const { Server } = require('http');
+Server((req, res) => {
+console.log(req.url);
+    let body = '';
+    req.on('data', data => body += data);
+    req.on('end', _ => {
+        if(req.url === '/result4/') {
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Method': 'GET,POST,PUT,DELETE,OPTIONS',
+                'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept,Access-Control-Allow-Headers',
+            });
+            res.write(JSON.stringify({
+                'message': 'the3rd',
+                'x-result': req.headers['x-test'],
+                'x-body': body,
+            }));
+            res.end();
+        }
+    });
+}).listen(3000); 
